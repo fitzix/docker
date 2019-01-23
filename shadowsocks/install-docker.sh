2b4bbc983597927dad02129935a84609573d03fd
fitzix <caojunkaiv@gmail.com>
2019-01-23 17:18:57 +0800

    feat(ss-mgr):

--- shadowsocks/install-docker.sh
+++ shadowsocks/install-docker.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/sh
 
-apt-get remove docker docker-engine docker.io containerd runc
+apt remove docker docker-engine docker.io containerd runc
 
-apt-get update
+apt update
 
-apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-apt-key fingerprint 0EBFCD88
+apt fingerprint 0EBFCD88
 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-apt-get update
+apt update
 
-apt-get install docker-ce
+apt install -y docker-ce
