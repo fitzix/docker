8af84f2358d6093e05c175888aa502d34781b816
fitzix <caojunkaiv@gmail.com>
2019-01-29 19:22:45 +0800

    fix(ss-mgr):

--- shadowsocks/install-docker.sh
+++ shadowsocks/install-docker.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Color
 
@@ -52,7 +52,7 @@ check_docker
 check_docker_compose
 
 echo "download compose file from fitzix github"
-wget "https://raw.githubusercontent.com/fitzix/docker/master/shadowsocks/docker-compose.yml" -O docker-compose.yml
+wget "https://raw.githubusercontent.com/fitzix/docker/master/shadowsocks/docker-compose.yml" -Oq docker-compose.yml
 
 read -p '是否安装webgui? (y/n) ' is_install_webgui
 
